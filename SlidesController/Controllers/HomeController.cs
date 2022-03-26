715408d62ee9560e72ad1cfc91856f936ed7bec4
Omar Muscatello <7016897+omaxel@users.noreply.github.com>
2022-03-26 22:37:06 +0100

    Initial commit

---
+++ SlidesController/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WindowsInput;
+using WindowsInput.Native;
+
+namespace SlidesController.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        private readonly InputSimulator sim = new InputSimulator();
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Previous()
+        {
+            sim.Keyboard.KeyPress(VirtualKeyCode.LEFT);
+            return Ok();
+        }
+
+        public IActionResult Next()
+        {
+            sim.Keyboard.KeyPress(VirtualKeyCode.RIGHT);
+            return Ok();
+        }
+    }
+}
